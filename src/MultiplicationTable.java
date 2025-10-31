b5d7077c414514de03e1b777c93740cfa368cb00
rodrigo <rodrigovelezdiaz@gmail.com>
2025-10-31 18:43:12 -0500

    creado archivo MultiplicationTable

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Multiplication table for " + number + ":");
+
+        // TODO: Add a for loop here to print the multiplication table
+    }
+}
