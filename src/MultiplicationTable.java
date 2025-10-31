bcc9a6331b0496753a19c6539e129593801a5463
rodrigo <rodrigovelezdiaz@gmail.com>
2025-10-31 18:43:55 -0500

    loop for implementado

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -3,6 +3,10 @@ public class MultiplicationTable {
         int number = 5;
         System.out.println("Multiplication table for " + number + ":");
 
-        // TODO: Add a for loop here to print the multiplication table
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " x " + i + " = " + result);
+        }
+
     }
 }
