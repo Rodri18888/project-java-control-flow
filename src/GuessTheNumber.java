96e55e50ac82d853e3fd113bd4a606c1b4cd8d84
rodrigo <rodrigovelezdiaz@gmail.com>
2025-10-31 18:47:39 -0500

    bucle while implementado

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -11,7 +11,20 @@ public class GuessTheNumber {
 
         System.out.println("I'm thinking of a number between 1 and 100. Can you guess it?");
 
-        // TODO: Add a while loop here to implement the guessing game
+        while (true) {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                break;
+            }
+        }
 
         scanner.close();
     }
